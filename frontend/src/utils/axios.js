60e9b6be534cb2777fcae8272158a064c844483a
Warsame Adam <warsameadam16@gmail.com>
2025-02-27 08:42:13 +0500

    auth setup

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_BACKEND_URL;
+
+const instance = axios.create({
+  baseURL: baseURL + "/api",
+});
+
+// Function to set up interceptors, receiving a function that retrieves the token
+export const setupAxiosInterceptors = (getToken) => {
+  instance.interceptors.request.use(
+    async (config) => {
+      try {
+        const token = await getToken(); // Get the latest token dynamically
+
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      } catch (error) {
+        console.error("Error fetching token:", error);
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+};
+
+export default instance;
